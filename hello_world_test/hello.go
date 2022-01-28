d8efbe87372bc2b6e12d6faedbaa9c43deab879f
Nicolas R Chinaglia <nicolas.chinaglia-etx@ab-inbev.com>
2022-01-28 15:34:16 -0300

    hello world with tests

---
+++ hello_world_test/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Função Hello que retorna uma string "Hello, World"
+func Hello() string {
+	return "Hello, World"
+}
+
+// Função main chama a função Hello()
+func main() {
+	fmt.Println(Hello())
+}
