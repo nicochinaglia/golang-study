beef0e2d6a37dd0638a2a282e73c355d58687014
Nicolas <chinaglianr@gmail.com>
2022-02-09 20:54:03 -0300

    aprendendo variaveis em go

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+# Variáveis
+(09/02/2022)
+
+# Hello world
+Nicolas Roberto Chinaglia - (25/01/2022)
+
 # golang-study
 Repository used to study and improve Golang skills
---
+++ variaveis/var_hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variável de texto.
+	var nome string = "Nicolas"
+
+	// Variável de número inteiro.
+	var idade int = 28
+
+	// Variável de verdadeiro ou falso.
+	var estuda bool = true
+
+	// Variável de número não inteiro.
+	var versao float32 = 0.1
+
+	fmt.Println("Olá, me chamo ", nome, "!\n Minha idade é: ", idade, ".\n Atualmente estou estudando? ", estuda, ".\n Qual a versão desse código? ", versao)
+}
