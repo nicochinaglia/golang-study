b352519d734db4a5c2e04fc247c09475ed0d34b5
Nicolas <chinaglianr@gmail.com>
2022-02-09 21:05:59 -0300

    atribuicao e criacao de variaveis

--- variaveis/var_hello.go
+++ variaveis/var_hello.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+// Pacote para retornar o tipo da variável
 
 func main() {
 
 	// Variável de texto.
+	// Pode ser declarada sem string, exemplo: var nome = "Nicolas".
 	var nome string = "Nicolas"
 
-	// Variável de número inteiro.
-	var idade int = 28
+	// Pode-se usar o nome da variável com := valor para criar a variável e já atribuir um valor. Por padrão o Go já identifica o tipo da variável. Variável de número inteiro.
+	idade := 28
 
 	// Variável de verdadeiro ou falso.
 	var estuda bool = true
@@ -16,5 +22,10 @@ func main() {
 	// Variável de número não inteiro.
 	var versao float32 = 0.1
 
+	var nomeCompleto = "Nicolas Roberto Chinaglia"
+
 	fmt.Println("Olá, me chamo ", nome, "!\n Minha idade é: ", idade, ".\n Atualmente estou estudando? ", estuda, ".\n Qual a versão desse código? ", versao)
+
+	//reflec.TypeOf(variável) - Retorna o tipo da variável selecionada.
+	fmt.Println("Podemos usar a variável sem declarar seu tipo. Por exemplo: a variável nomeCompleto é do tipo:", reflect.TypeOf(nomeCompleto))
 }
